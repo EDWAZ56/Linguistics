50af1675d60fe78794d7c8d9730ba58d5875c67a
Eduard Dauwe <Eduard.Dauwe@realdolmen.com>
2018-06-26 14:59:20 +0200

    huidige toestand

--- antecedentsearch.py
+++ antecedentsearch.py
@@ -6,45 +6,69 @@ text=open(r'C:\Users\EDWAZ56\Desktop\pythonantecedent\1000-passion-bfm-p.txt','r
 #ken variabele toe aan tekst
 tekst=text.read()
 #print volledige tekst
-print tekst
+#print tekst
 #zoek alle zelfstandige naamwoorden in tekst
-nouns=re.finditer('\(NP-SBJ \(([a-z]*)',tekst)
+nouns=re.finditer('NCS ([A-Za-z]*)\)',tekst)
 #print matchobject met alle zelfstandige naamwoorden
 print nouns
 
 #overloop alle zelfstandige naamwoorden in tekst
+counter=0
+nouncounter=0
 for noun in nouns:
     #print matchobject van zelfstandig naamwoord
-    print noun
-    #print zelfstandig naamwoord
-    print noun.group(1)
+    #print noun
+    
     #positie van het zelfstandig naamwoord in de tekst
     pos=noun.span()[1]
-    #print positie van het werkwoord
-    print pos
+    #print positie van het zelfstandig naamwoord
+    #print pos
     #herdefinieer tekst als tekst beginnend vanaf eindpositie zelfstandig naamwoord
     subtekst=tekst[pos:]
-    print subtekst
+    #print subtekst
     
     #zoek alle werkwoorden in subtekst
-    
+    verbs=re.finditer('VJ ',subtekst)
     #print matchobject met alle werkwoorden
-    
+    #print verbs
+    #initiate cardinality, set cardinality to 0
+    cardinality=0
+    #initiate verbpos
+    verbposold=0
     #overloop alle werkwoorden in subtekst
+    nouncounter+=1
+    for verb in verbs:
             #houd counter bij
-                    
+            cardinality+=1
             #print werkwoord
-            
+            #print verb
+            #print verb.group()
             #positie van het werkwoord in subtekst
-           
-           
+            verbposnew=verb.span()[1]-verbposold
+            
             #herdefinieer subtekst als tekst beginnend vanaf laatste eindpositie en eindigend bij positie werkwoord
+            subtekst=subtekst[verbposold:verbposnew]
             
+            #redefine verbposold
+            verbposold=verbposnew          
             #zoek naar zelfstandig naamwoord in subtekst
-            
+            #print noun.group(1)
+            nounoccurence=noun.group(1)
+            regex='NCS (%s)\)' %(nounoccurence) 
+            nouncopy=re.search(regex,subtekst)
             #indien gevonden, ga uit binnenste loop en ga naar volgend zelfstandig naamwoord en geef counter weer
+            if nouncopy is not None:
+                print subtekst
+                print cardinality
+                nouncopy=noun.group(1)
+                print 'The noun %s was found after verb number %s' %(nouncopy,cardinality)
+                counter+=1
+                break
+            else:
+                continue
+print counter
+print nouncounter
 
-            
 
 '''#zoek volgende woord pastoor op regel
 prog=re.compile('pastoor')
